d83275366e889c7a9902eb8341b461c37c939c31
Pedro Paulo <pedropaulotj@gmail.com>
2023-03-13 17:12:01 -0300

    fix

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -66,10 +66,6 @@ function useFetch<T>(
           method,
           headers: {
             "Content-Type": options.contentType || "application/json",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-            "Access-Control-Allow-Headers":
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
           },
         } as AxiosRequestConfig;
 
